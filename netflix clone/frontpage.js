5747e06e66a3c77bdaf80854749a2d1ceba80efa
MansiTipre <160388232+MansiTipre@users.noreply.github.com>
2024-03-29 19:13:50 +0530

    Add files via upload

---
+++ netflix clone/frontpage.js
@@ -0,0 +1,9 @@
+
+
+// let ele=document.getElementsByClassName("para");
+// function getClick1(params) {
+//     // alert("clicked")
+//     ele[0].style.height="300px"
+//     ele[0].style.width="900px"
+//     ele[0].style.backgeoundColor="rgb(58,51,51)";
+// }
